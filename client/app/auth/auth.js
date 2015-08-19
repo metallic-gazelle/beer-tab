5399e8c7238c710740d589345e93b68995949fcb
Chris Saden <csaden@gmail.com>
2015-08-19 11:53:25 -0700

    add checkRequired directive for over 21

--- client/app/auth/auth.js
+++ client/app/auth/auth.js
@@ -1,7 +1,7 @@
 var auth = angular.module('beer-tab.auth', []);
 
 auth.controller('AuthCtrl', function ($scope, $rootScope, $window, $location, AuthService) {
-  
+
   $scope.user = {};
   $scope.logIn = function () {
     $window.username = $scope.user.username;
@@ -30,9 +30,22 @@ auth.controller('AuthCtrl', function ($scope, $rootScope, $window, $location, Au
     AuthService.signout();
   };
 
-  $scope.redirect = function(path) {
+  $scope.redirect = function (path) {
     console.log(path);
     $location.path('/' + path);
   };
 
+})
+.directive('checkRequired', function () {
+  return {
+    require: 'ngModel',
+    restrict: 'A',
+    link: function (scope, element, attrs, ngModel) {
+      ngModel.$validators.checkRequired = function (modelValue, viewValue) {
+        var value = modelValue || viewValue;
+        var match = scope.$eval(attrs.ngTrueValue) || true;
+        return value && match === value;
+      };
+    }
+  };
 });
