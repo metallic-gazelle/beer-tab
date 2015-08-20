7fa8647fcee8a2884d23114566d6ba14a1ce0c9f
Chris Saden <csaden@gmail.com>
2015-08-20 15:39:09 -0700

    fix syntax and add back search functions

--- client/app/main/main.js
+++ client/app/main/main.js
@@ -7,15 +7,15 @@ main.controller('MainCtrl', function ($scope, $window, beerPmt, jwtHelper, AuthS
   // Decode token (this uses angular-jwt. notice jwtHelper)
   $scope.decodedJwt = $scope.jwt && jwtHelper.decodeToken($scope.jwt);
   // Object used to contain user's beer network
-  
 
-  $scope.getTable = function(){
-    getTable.getTable($scope.username) 
+
+  $scope.getTable = function () {
+    getTable.getTable($scope.username)
       .then(function (derp) {
         $scope.network = util.toArr(derp);
         console.log(derp);
       });
-    };
+  };
 
 
   // $scope.network =  argle || $scope.decodedJwt.network;
@@ -28,31 +28,58 @@ main.controller('MainCtrl', function ($scope, $window, beerPmt, jwtHelper, AuthS
   // new friend form
   $scope.clicked = false;
   /*$scope.network = [];*/
-  
-  //This function sends a request to the server, it returns 
+
+  //This function sends a request to the server, it returns
   //the updated information
   $scope.sendBeer = function (user) {
-    
-    if(user){
+
+    if (user) {
       console.log('sendBeer called', user);
-      
-      if(AuthService.isAuth()) {
+
+      if (AuthService.isAuth()) {
         beerPmt.newIOU(user)
-        .then(function(derp){
-          console.log(derp); 
+        .then(function (derp) {
           $scope.network = util.toArr(derp.network);
-          
+
         });
       }
     }
   };
+
+  $scope.findUser = function (inputStr) {
+    $scope.results = [];
+    beerPmt.findUsers().then(function (data) {
+      if (inputStr.length > 0) {
+        for (var i = 0; i < data.length; i++) {
+          if (data[i].name.first.toLowerCase().match(inputStr.toLowerCase()) !== null || data[i].name.last.toLowerCase().match(inputStr.toLowerCase()) !== null) {
+            $scope.results.push({name: data[i].name.first + ' ' + data[i].name.last, username: data[i].username});
+          }
+        }
+      }
+    });
+  };
+
+  $scope.toggle = function () {
+    if ($scope.clicked === false) {
+      $scope.clicked = true;
+    } else {
+      $scope.clicked = false;
+    }
+  };
+
+  $scope.clearField = function () {
+    $scope.toUser = '';
+    $scope.results = [];
+  };
+
 });
 
-main.filter('range', function() {
-  return function(input, total) {
-    total = Math.abs(parseInt(total));
-    for (var i=0; i<total; i++)
+main.filter('range', function () {
+  return function (input, total) {
+    total = Math.abs(parseInt(total, 10));
+    for (var i = 0; i < total; i++) {
       input.push(i);
+    }
     return input;
   };
 });
